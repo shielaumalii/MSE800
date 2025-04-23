560d4cadd359d85aa79de874173689dde13847b1
shielaumalii <270650805@yoobeestudent.ac.nz>
2025-04-23 15:19:19 +1200

    Create MSE800_Activity_Array-of-Positive-Integers.py

---
+++ MSE800_Activity_Array-of-Positive-Integers.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Create a NumPy array and perform operations
+array = np.arange(1, 11)
+print("Array:", array)
+print("Shape:", array.shape, "| Data Type:", array.dtype)
+print("Array Multiplied by 2:", array * 2)
