2cd656e23afc25d4d014d384fc80ac39192e78c8
shielaumalii <270650805@yoobeestudent.ac.nz>
2025-05-10 10:50:30 +1200

    Update database.py

--- Week5/database_exercise/database.py
+++ Week5/database_exercise/database.py
@@ -1,18 +1,31 @@
 import sqlite3
 
-def create_connection():
-    conn = sqlite3.connect("users.db")
+def create_connection(db_name):
+    conn = sqlite3.connect(db_name)
     return conn
 
-def create_table():
-    conn = create_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
+def create_tables():
+    conn_users = create_connection("users.db")
+    cursor_users = conn_users.cursor()
+    cursor_users.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             email TEXT NOT NULL UNIQUE
         )
     ''')
-    conn.commit()
-    conn.close()
+    conn_users.commit()
+    conn_users.close()
+
+  
+    conn_courses = create_connection("course.db")
+    cursor_courses = conn_courses.cursor()
+    cursor_courses.execute('''
+        CREATE TABLE IF NOT EXISTS course (
+            course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            course_name TEXT NOT NULL,
+            course_unit INTEGER NOT NULL
+        )
+    ''')
+    conn_courses.commit()
+    conn_courses.close()
