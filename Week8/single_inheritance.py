28bae6d53428cdcefb3ad4fc23f0e23085ff62c6
shielaumalii <270650805@yoobeestudent.ac.nz>
2025-06-01 11:31:50 +1200

    update

--- Week8/single_inheritance.py
+++ Week8/single_inheritance.py
@@ -6,11 +6,13 @@ class Animal:
 # Child class inheriting from Animal
 class Dog(Animal):
     def bark(self):
-        return "The dog barks"
+        return "Dog barks 'Woof Woof!'"
 
 # Create an instance of Dog
 my_dog = Dog()
+my_dog.speak()  # Call method from parent class
+my_dog.bark()   # Call method from child class
 
-# Call methods from both parent and child classes
-print(my_dog.speak())  # Inherited from Animal
-print(my_dog.bark())   # Defined in Dog
+# # Call methods from both parent and child classes
+# print(my_dog.speak())  # Inherited from Animal
+# print(my_dog.bark())   # Defined in Dog
