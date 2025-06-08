5164b93fcd18f80f5cedff1d4f21a635bce55902
shielaumalii <270650805@yoobeestudent.ac.nz>
2025-06-08 12:26:10 +1200

    saved

--- Week9/debug1.py
+++ Week9/debug1.py
@@ -1,4 +1,4 @@
 # for i in range(1, 20):
-for i in range(1, 21):
+for i in range(1, 21): # Changed range to include 20
     if i == 20:
         print(f"You got it!")
--- Week9/debug4.py
+++ Week9/debug4.py
@@ -1,7 +1,7 @@
 # example 4
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: ")) # from comparison to assignment
 total_words = pages * word_per_page
 
 print(f"We have {total_words} in total.")
